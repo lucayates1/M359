374e08617b8f53979b609bafc1b545251714fba9
Luca J Yates <yates1268@students.d211.org>
2024-09-26 13:48:02 -0500

    fixed most of the restaurant class finsihsed most of it just some formating errors elft and discounts to add

--- M359_Project/src/U3/lab/RestaurantOrder.java
+++ M359_Project/src/U3/lab/RestaurantOrder.java
@@ -1,7 +1,7 @@
 package U3.lab;
 
 public class RestaurantOrder {
-    private String currOrder;
+    private String currOrder = "";
     private String customerName;
     private boolean discount;
     private double discountPercent;
@@ -29,24 +29,27 @@ public class RestaurantOrder {
     }
 
     public void addItem(String itemName, int quantity){
-        if(itemName.equalsIgnoreCase("burger")){
+        if(itemName.equalsIgnoreCase("burger") && quantity > 0 ){
             burgerCount += quantity;
-            currOrder += "Ordered " + quantity +  itemName + "(s) \n";
+            currOrder += "Ordered " + quantity +  " " + itemName + "(s) \n";
         }
-        if(itemName.equalsIgnoreCase("pizza")){
+        if(itemName.equalsIgnoreCase("pizza") && quantity > 0){
             pizzaCount+= quantity;
-            currOrder += "Ordered " + quantity +  itemName + "(s) \n";
+            currOrder += "Ordered " + quantity + " " +  itemName + "(s) \n";
         }
-        if(itemName.equalsIgnoreCase("pasta")){
+        if(itemName.equalsIgnoreCase("pasta") && quantity > 0){
             pastaCount+= quantity;
-            currOrder += "Ordered " + quantity +  itemName + "(s) \n";
+            currOrder += "Ordered " + quantity +  " " +  itemName + "(s) \n";
         }
-        if(itemName.equalsIgnoreCase("salad")){
+        if(itemName.equalsIgnoreCase("salad") && quantity > 0){
             saladCount+= quantity;
-            currOrder += "Ordered " + quantity +  itemName + "(s) \n";
+            currOrder += "Ordered " + quantity +  " " +  itemName + "(s) \n";
         }
 
-        if(!itemName.equalsIgnoreCase("BURGER || PIZZA || PASTA || SALAD")){
+        if(!itemName.equalsIgnoreCase("BURGER" ) && !itemName.equalsIgnoreCase("PIZZA" ) && !itemName.equalsIgnoreCase("PASTA" )
+        && !itemName.equalsIgnoreCase("SALAD" ))
+        {
+
             currOrder += "sorry," + itemName + " is not on the menu\n";
         }
         if(quantity <= 0){
@@ -56,13 +59,16 @@ public class RestaurantOrder {
 
     public String toString(){
         double subTotal = saladCount * SALADCOST + pastaCount * PASTACOST + burgerCount * BURGERCOST + pizzaCount * PIZZACOST;
-        double taxAmount = subTotal / taxPercent;
+        double taxAmount = subTotal * taxPercent;
         String fOutput = "------ Order for: " + customerName + " ------\n";
         fOutput += currOrder + "\n";
-        fOutput += "Subtotal:\t$" + subTotal  +"\t";
-        fOutput += "Tax\t" + "(" + taxPercent + "%):\t$" + taxAmount;
-        fOutput += "Total:\t\t "  + (subTotal - taxAmount);
-        fOutput += "------------------------------";
+        fOutput += "Subtotal:\t\t$" + subTotal  +"\t\n";
+        if(discount){
+            fOutput += "\t Discount:\t" + discountPercent;
+        }
+        fOutput += "Tax\t" + "(" + taxPercent * 100 + "%):"+ "\t" +"$" + taxAmount + "\n";
+        fOutput += "Total:\t\t\t$"  + (subTotal - taxAmount);
+        fOutput += "\n------------------------------" + "\t\t\t\t\t";
 
 
 
